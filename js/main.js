c30e0de609b4d9dbed907884565664eca7c331de
RODRIGO BERGENTHAL <116042656+RodrigoBergenthal@users.noreply.github.com>
2024-05-30 16:41:23 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-  $(document).ready(function () {
+    $(document).ready(function () {
             // Inicializa o carrossel
             $('#carousel-imagens').slick({
                 autoplay: true,
